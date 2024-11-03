5d4628a3a0ea7f76d32bb284a71d8c5b2e8193ba
Kim Hong Giang <giangkh1908@gmail.com>
2024-11-04 01:31:19 +0700

    test mixin

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import MainLayout from '@component/Layout/Layout'
 import MyHeader from '@component/Header/Header'
 import MyFooter from '@component/Footer/Footer'
+import MyButton from './components/Button/Button';
 
 
 function App() {
   return (
     <>
       <MainLayout>
-        <MyHeader />
+        {/* <MyHeader />
         Content
-        <MyFooter />
+        <MyFooter /> */}
+        <MyButton />
       </MainLayout>
     </>
   );
