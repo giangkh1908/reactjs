a8cec2006764408b2816df19b9198453184e6f1a
Kim Hong Giang <giangkh1908@gmail.com>
2024-11-27 15:08:01 +0700

    config routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-
 import Homepage from '@components/HomePage/HomePage';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import routers from '@/routers/routers';
+import { Suspense } from 'react';
 
 function App() {
   return (
-    <>
-      <Homepage />
-    </>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}> 
+      {/* hiệu ứng loading */}
+        <Routes>
+          {
+            routers.map((item, index) => {
+              return (
+                <Route
+                  key={index}
+                  path={item.path}
+                  element={<item.component />} />
+              );
+            })}
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
