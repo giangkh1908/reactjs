bd46d4194b6be67c088a15560b81f11cea0c3949
giangkh1908 <giangkh1908@gmail.com>
2024-12-24 01:01:19 +0700

    Done Login UI UX

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -3,6 +3,7 @@ import styles from "./styles.module.scss";
 import { SideBarContext } from "@/contexts/SideBarProvider";
 import classNames from "classnames";
 import { TfiClose } from 'react-icons/tfi';
+import Login from "@components/ContentSideBar/Login/Login";
 function SideBar() {
   const { overlay, container, sideBar, slideSideBar, boxIcon } = styles;
 
@@ -48,7 +49,7 @@ function SideBar() {
             <TfiClose />
           </div>
         )}
-
+        <Login />
         {/* {handleRenderContent()} */}
       </div>
     </div>
